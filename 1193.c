0984327eea49634566e2c45d2a4d26912c2a5193
Metna1 <59068240+Metna1@users.noreply.github.com>
2022-01-24 22:40:05 +0900

    백준 1193번

--- 1193.c
+++ 1193.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, d, k;
+	int i, k, j=1, n, s=1, a=1;
 	
-	scanf("%d", &a);
-
-	for (int i=0; i<k; i++) {
-
-
+	scanf("%d", &n);
+	for (k=1; k<n && j!=n; k+=2){
+		for (i=1; i<k && j<n; i++ && j++) {
+			printf("%d", i);
+		}
+		for (; i>0 && j<n; i-- && j++) {
+			printf("%d", i);
+		}
 	}
+	printf("\n%d/\n", i);
+	
+	j=1;
+	for (k=2; k<n && j<=n; k+=2){
+		for (i=1; i<k && j<n; i++ && j++) {
+			printf("%d", i);
+		}
+		for (; i>0 && j<n; i-- && j++) {
+			printf("%d", i);
+		}
+	}
+	printf("\n%d\n", i);
 
 	return 0;
 }

4da1dbd0c818097cc560cc8c459d896dbb7c409e
Metna1 <59068240+Metna1@users.noreply.github.com>
2022-01-27 21:34:31 +0900

    백준 2275번

--- 2275.c
+++ 2275.c
@@ -1,29 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	int i, k, j=1, n, s=1, a=1;
-	
-	scanf("%d", &n);
-	for (k=1; k<n && j!=n; k+=2){
-		for (i=1; i<k && j<n; i++ && j++) {
-			printf("%d", i);
-		}
-		for (; i>0 && j<n; i-- && j++) {
-			printf("%d", i);
-		}
-	}
-	printf("\n%d/\n", i);
-	
-	j=1;
-	for (k=2; k<n && j<=n; k+=2){
-		for (i=1; i<k && j<n; i++ && j++) {
-			printf("%d", i);
-		}
-		for (; i>0 && j<n; i-- && j++) {
-			printf("%d", i);
-		}
-	}
-	printf("\n%d\n", i);
+	int i, j, t, n, a, b;
 
 	return 0;
 }

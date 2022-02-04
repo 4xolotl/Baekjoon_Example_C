329ef7335ed4dfa7b9864f41db9002c09df8ddc6
Metna1 <59068240+Metna1@users.noreply.github.com>
2022-02-04 23:48:25 +0900

    백준 10757번

--- 10757.c
+++ 10757.c
@@ -1,71 +1,62 @@
 #include <stdio.h>
 
 int main() {
-	char a[10001], b[10001], p, la, lb;
+	char a[10010], b[10010], p, la, lb;
 	int i, m=0;
 	
-	for (i=0;;i++) {
+	for (i=1;;i++) {
 		scanf("%c", &p);
-		if (49 <= p && p <= 57) a[i] = p-48;
+		if (48 <= p && p <= 57) a[i] = p-48;
 		else if (p == ' ') {
 			la = i;
 			break;
 		}
 	}
-	for (i=0;;i++) {
+	for (i=1;;i++) {
 		scanf("%c", &p);
-		if (49 <= p && p <= 57) b[i] = p-48;
+		if (48 <= p && p <= 57) b[i] = p-48;
 		else {
 			lb = i;
 			break;
 		}
 	}
-
-	if (la < lb) {
-		for (i=0; i<la; i++) {
-			if (a[i] + b[i] > 9) {
-				if (b[i+1] < 10 && b[i+1] > 0) b[i-1] += 1;
+	a[0]=0;
+	b[0]=0;
+	if (la==lb) {
+		for (i=1; i<=lb; i++) {
+			if (a[la-i] + b[lb-i] > 9) {
+				if (b[lb-i-1] < 10 && b[lb-i-1] >= 0) b[lb-i-1] += 1;
 				else {
 					m=1;
-					b[i-1] = 1;
+					b[lb-i-1] = 1;
 				}
+			}
+			else b[lb-i] = (a[la-i] + b[lb-i]) % 10;
+		}
+		if (m==1) for (i=0; i<lb+1; i++) printf("%d", b[i]);
+		else for (i=1; i<lb; i++) printf("%d", b[i]);
+	}
+	else if (la < lb) {
+		for (i=0; i<=la; i++) {
+			if (a[la-i] + b[lb-i] > 9) {
+				b[lb-i-1] += 1;
 			}
 			else {
-				b[i] = (a[i] + b[i]) % 10;
+				b[lb-i] = (a[la-i] + b[lb-i]) % 10;
 			}
 		}
-		if (m==1) for (i=0; i<lb+1; i++) printf("%d", b[i]);
-		else for (i=0; i<lb; i++) printf("%d", b[i]);
-	}
-	else if (la==lb) {
-		for (i=0; i<lb; i++) {
-			if (a[i] + b[i] > 9) {
-				if (b[i+1] < 10 && b[i+1] > 0) b[i-1] += 1;
-				else {
-					m=1;
-					b[i-1] = 1;
-				}
-			}
-			else b[i] = (a[i] + b[i]) % 10;
-		}
-		if (m==1) for (i=0; i<lb+1; i++) printf("%d", b[i]);
-		else for (i=0; i<lb; i++) printf("%d", a[i]);
+		for (i=0; i<lb; i++) printf("%d", b[i]);
 	}
 	else if (lb < la) {
-		for (i=0; i<lb; i++) {
-			if (a[i] + b[i] > 9) {
-				if (b[i+1] < 10 && b[i+1] > 0) b[i-1] += 1;
-				else {
-					m=1;
-					b[i-1] = 1;
-				}
+		for (i=0; i<=lb; i++) {
+			if (a[la-i] + b[lb-i] > 9) {
+				a[la-i-1] += 1;
 			}
 			else {
-				a[i] = (a[i] + b[i]) % 10;
+				a[la-i] = (a[la-i] + b[lb-i]) % 10;
 			}
 		}
-		if (m==1) for (i=0; i<la+1; i++) printf("%d", a[i]);
-		else for (i=0; i<la; i++) printf("%d", a[i]);
+		for (i=0; i<la; i++) printf("%d", a[i]);
 	}
 	return 0;
 }

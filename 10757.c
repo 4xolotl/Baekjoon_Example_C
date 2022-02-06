40896c2d9dd8fa6fd84b216c6bb3039ac89643ba
Metna1 <59068240+Metna1@users.noreply.github.com>
2022-02-06 23:53:23 +0900

    Update 10757.c

--- 10757.c
+++ 10757.c
@@ -23,40 +23,45 @@ int main() {
 	a[0]=0;
 	b[0]=0;
 	if (la==lb) {
-		for (i=1; i<=lb; i++) {
+		for (i=0; i<lb; i++) {
 			if (a[la-i] + b[lb-i] > 9) {
-				if (b[lb-i-1] < 10 && b[lb-i-1] >= 0) b[lb-i-1] += 1; //반올림 함수 만들기
-				else {
+				if (i==lb-1) {
 					m=1;
-					b[lb-i-1] = 1;
 				}
+				b[lb-i-1] += 1;
 			}
-			else b[lb-i] = (a[la-i] + b[lb-i]) % 10;
+			b[lb-i] = (a[la-i] + b[lb-i]) % 10;
 		}
-		if (m==1) for (i=0; i<lb+1; i++) printf("%d", b[i]);
+		if (m==1) for (i=0; i<lb; i++) printf("%d", b[i]);
 		else for (i=1; i<lb; i++) printf("%d", b[i]);
 	}
 	else if (la < lb) {
-		for (i=0; i<=la; i++) {
+		for (i=0; i<la; i++) {
 			if (a[la-i] + b[lb-i] > 9) {
+				if (i==la-1) {
+					m=1;
+				}
 				b[lb-i-1] += 1;
 			}
-			else {
-				b[lb-i] = (a[la-i] + b[lb-i]) % 10;
-			}
+			b[lb-i] = (a[la-i] + b[lb-i]) % 10;
+			
 		}
-		for (i=0; i<lb; i++) printf("%d", b[i]);
+		if (m==1) for (i=0; i<lb; i++) printf("%d", b[i]);
+		else for (i=1; i<lb; i++) printf("%d", b[i]);
 	}
 	else if (lb < la) {
-		for (i=0; i<=lb; i++) {
+		for (i=0; i<lb; i++) {
 			if (a[la-i] + b[lb-i] > 9) {
+				if (i==lb-1) {
+					m=1;
+				}
 				a[la-i-1] += 1;
 			}
-			else {
-				a[la-i] = (a[la-i] + b[lb-i]) % 10;
-			}
+			a[la-i] = (a[la-i] + b[lb-i]) % 10;
+			
 		}
-		for (i=0; i<la; i++) printf("%d", a[i]);
+		if (m==1) for (i=0; i<la; i++) printf("%d", a[i]);
+		else for (i=1; i<la; i++) printf("%d", a[i]);
 	}
 	return 0;
 }

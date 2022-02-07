b659ee2297911a7519928cbb6a139a65fd10b831
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-02-07 15:02:19 +0900

    Update 10757.c

--- 10757.c
+++ 10757.c
@@ -7,7 +7,7 @@ int main() {
 	for (i=1;;i++) {
 		scanf("%c", &p);
 		if (48 <= p && p <= 57) a[i] = p-48;
-		else if (p == ' ') {
+		else {
 			la = i;
 			break;
 		}
@@ -22,46 +22,42 @@ int main() {
 	}
 	a[0]=0;
 	b[0]=0;
+	if (la<lb) {
+		for (i=0; i<=lb-la; i++) 
+			p=a[i];
+			a[i+1]=p;
+		la=lb;
+	}
 	if (la==lb) {
-		for (i=0; i<lb; i++) {
+		for (i=0; i<=lb; i++) {
 			if (a[la-i] + b[lb-i] > 9) {
-				if (i==lb-1) {
-					m=1;
-				}
 				b[lb-i-1] += 1;
 			}
 			b[lb-i] = (a[la-i] + b[lb-i]) % 10;
-		}
-		if (m==1) for (i=0; i<lb; i++) printf("%d", b[i]);
-		else for (i=1; i<lb; i++) printf("%d", b[i]);
+		} for (i=0; i<lb; i++) printf("%d", b[i]);
 	}
 	else if (la < lb) {
-		for (i=0; i<la; i++) {
-			if (a[la-i] + b[lb-i] > 9) {
-				if (i==la-1) {
-					m=1;
+		for (i=0; i<=lb; i++) {
+			if (i<=la) {
+				if (a[la-i] + b[lb-i] > 9) {
+					b[lb-i-1] += 1;
 				}
-				b[lb-i-1] += 1;
+				b[lb-i] = (a[la-i] + b[lb-i]) % 10;
 			}
-			b[lb-i] = (a[la-i] + b[lb-i]) % 10;
-			
-		}
-		if (m==1) for (i=0; i<lb; i++) printf("%d", b[i]);
-		else for (i=1; i<lb; i++) printf("%d", b[i]);
+			else {
+				if(b[lb-1]>9) b[lb-i-1] += 1;
+			}
+		}for (i=1; i<lb; i++) printf("%d", b[i]);
 	}
 	else if (lb < la) {
-		for (i=0; i<lb; i++) {
+		for (i=0; i<lb+1; i++) {
 			if (a[la-i] + b[lb-i] > 9) {
-				if (i==lb-1) {
-					m=1;
-				}
 				a[la-i-1] += 1;
 			}
 			a[la-i] = (a[la-i] + b[lb-i]) % 10;
 			
 		}
-		if (m==1) for (i=0; i<la; i++) printf("%d", a[i]);
-		else for (i=1; i<la; i++) printf("%d", a[i]);
+		for (i=1; i<la; i++) printf("%d", a[i]);
 	}
 	return 0;
 }

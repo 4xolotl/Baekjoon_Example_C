bdc7fa9fae5dc4debb27801be2b6681a21481388
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-02-08 14:30:14 +0900

    Update 10757.c

--- 10757.c
+++ 10757.c
@@ -6,35 +6,39 @@ int main() {
 	
 	for (i=1;;i++) {
 		scanf("%c", &p);
-		if (48 <= p && p <= 57) a[i] = p-48;
+		if (48<=p && p<=57) a[i]=p-48;
 		else break;
 	}
-	la = i;
+	la=i-1;
 	for (i=1;;i++) {
 		scanf("%c", &p);
-		if (48 <= p && p <= 57) b[i] = p-48;
+		if (48<=p && p<=57) b[i]=p-48;
 		else break;
 	}
-	lb = i;
+	lb=i-1;
 	a[0]=0;
 	b[0]=0;
 	if (la<lb) {
 		m=lb-la;
-		for (i=0; i<m; i++) a[lb-i-2]=a[lb-i-2];
-		for (i=0; i<m; i++) a[i]=0;
+		for (i=0; i<=m; i++) a[lb-i]=a[la-i];
+		for (i=0; i<=m; i++) a[i]=0;
 		la=lb;
 	}
 	else if (lb<la) {
 		m=la-lb;
-		for (i=0; i<m; i++) b[la-i]=b[la-i];
-		for (i=0; i<m; i++) b[i]=0;
+		for (i=0; i<=m; i++) b[la-i]=b[lb-i];
+		for (i=0; i<=m; i++) b[i]=0;
 		lb=la;
-	}
-	for (i=0; i<=lb; i++) {
+	} m=0;
+	for (i=0; i<lb; i++) {
 		if (a[la-i] + b[lb-i] > 9) {
+			if (i==lb-1) m=1;
 			b[lb-i-1] += 1;
 		}
-		b[lb-i] = (a[la-i] + b[lb-i]) % 10;
-	} for (i=0; i<lb; i++) printf("%d", b[i]);
+		b[lb-i]=(a[la-i]+b[lb-i])%10;
+	}
+	if (m==1) i=0;
+	else i=1;
+	for (;i<=lb; i++) printf("%d", b[i]);
 	return 0;
 }

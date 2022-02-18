c2157a2f7bd771d14818e62b3e145b2e931f4aa6
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-02-18 18:06:55 +0900

    Update 1929.c

--- 1929.c
+++ 1929.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main() {
-	int m, n, j, k, i, a[1000001];
-	
+	int m, n, j, k, i, a[100000];
+
 	scanf("%d%d", &m, &n);
-	for (i=m;i<=n;i++) a[i]=i;
+	
+	for (i=m; i<=n; i++) a[i]=i;
 
 	for (i=2; i<n/2; i++) {
-		for (j=2;j<n/i;j++) if (i%j==0) a[i]=0;
+		for (j=2; j<=n; j++) a[i*j]=0;
 	}
-	for (i=m;i<=n;i++) if (a[i]!=0 )printf("%d", a[i]=i);
-	
+	for (i=m; i<=n; i++) if (a[i]!=0) printf("%d\n", a[i]);
+
 	return 0;
 }

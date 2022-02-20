42bc528d6b1232d6d9a15e0f77d1efd479bc226b
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-02-21 03:02:42 +0900

    백준 1929번

--- 1929.c
+++ 1929.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int m, n, j, k=0, i, a[100000];
+	int m, n, j, i, a[100000];
 
 	scanf("%d%d", &m, &n);
 	
 	for (i=m; i<=n; i++) a[i]=i;
 
 	for (i=2; i<n/2; i++) {
-		for (j=2; j<=n; j++) 
-			if (i*j<=n && i*j>=m) a[i*j]=0;
+		for (j=i; j<=n; j++) 
+			if (i*j<=n && i*j>=m) {
+				a[i*j]=0;
+			}
 	}
 	if (m==1) m=2;
 	for (i=m; i<=n; i++) if (a[i]!=0) printf("%d\n", a[i]);

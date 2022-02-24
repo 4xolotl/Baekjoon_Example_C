3939f6ab4fda6f368e0f8375efc1ddd48c1a993b
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-02-24 14:50:08 +0900

    Create 3009.c

---
+++ 3009.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int x[5], y[5], i, a=0, b=0;
+
+	for (i=3; i>0; i--) {
+		if (a==x[i])
+		scanf("%d%d", &x[i], &y[i]);
+		a=x[i];
+		b=y[i];
+	}
+	if (cnt=1) {
+
+	}
+
+
+	return 0;
+}

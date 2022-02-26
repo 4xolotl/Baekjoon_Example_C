44658c938dd4dad7a76f62af37ddf0e4726eb3d7
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-02-26 22:39:34 +0900

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int x1, y1,  x2, y2,  t, i;
+	double r1, r2;
+
+	scanf("%d");
+
+	for (i=0; i<t; i++) {
+		scanf("%d%d%lf%d%d%lf", &x1, &y1, &r1, &x2, &y2, &r2);
+
+
+	}
+
+	return 0;
+}

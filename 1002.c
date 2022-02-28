06333e70704d3237a99c10dd153ee681996eff37
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-02-28 22:49:55 +0900

    Update 1002.c

--- 1002.c
+++ 1002.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	int x1, y1,  x2, y2,  t, i;
-	double r1, r2;
+	int x1, y1, r1, x2, y2, r2, t, i;
 
-	scanf("%d");
+	scanf("%d", &t);
 
 	for (i=0; i<t; i++) {
-		scanf("%d%d%lf%d%d%lf", &x1, &y1, &r1, &x2, &y2, &r2);
-
+		scanf("%d%d%d%d%d%d", &x1, &y1, &r1, &x2, &y2, &r2);
+		if (((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))/(r1*r1+r2*r2)==) printf("1");
+		else if (((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))<) printf("0");
+		else if ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)>r1*r1+r2*r2) printf("2");
 	}
 
 	return 0;

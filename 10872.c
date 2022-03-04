33cde505bde6d6c3833bfa46635aa102ea73b67c
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-03-05 01:27:14 +0900

    백준 10872번

--- 10872.c
+++ 10872.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
-int p(int n, int d);
-
+int p(int n) {
+	if (n<=1) return 1;
+	return n*p(n-1);
+}
 int main() {
 	int n;
 
 	scanf("%d", &n);
-	printf("%d", p(n, 1));
-	return 0;
-}
+	printf("%d", p(n));
 
-int p(int n, int d) {
-	if (d==n) return d;
-	d*=n;
-	d++;
-	p(n, d);
+	return 0;
 }

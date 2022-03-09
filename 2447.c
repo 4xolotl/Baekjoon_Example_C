22a39ad23457294411ef19a9cbdd1472085a1e2f
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-03-09 18:55:07 +0900

    Update 2447.c

--- 2447.c
+++ 2447.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 int main() {
-	int n, k, a;
+	int n, k, a, i, k;
 
 	scanf("%d", &n);
+
+	for (k=0; k<n/3; k++) {
+		for (i=0;i<n/3; i++) {
+			printf("***");
+		}
+		printf("\n");
+		for (i=0;i<n/3; i++) {
+			printf("* *");
+		}
+		printf("\n");
+		for (i=0;i<n/3; i++) {
+			printf("***");
+		}
+	}
 
 	return 0;
 }

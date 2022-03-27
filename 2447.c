ca418e89c5eb268d83a6548b925d140ade43f662
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-03-27 22:06:18 +0900

    Update 2447.c

--- 2447.c
+++ 2447.c
@@ -1,21 +1,51 @@
 #include <stdio.h>
+#include <math.h>
+
+void sss(int n) {
+	int i;
+
+	for (i=0; i<n; i++) {
+		printf("***");
+	}
+}
+
+void sbs(int n) {
+	int i;
+
+	for (i=0; i<n; i++) {
+		printf("* *");
+	}
+}
+
+void bbb(int n) {
+	int i;
+
+	for (i=0; i<n; i++) {
+		printf("   ");
+	}
+}
 
 int main() {
-	int n, k, a, i, k;
+	int i, j, m, l=3;
+	float n, k;
 
-	scanf("%d", &n);
+	scanf("%f", &n);
+	k = log(n) / log(3.0);
 
-	for (k=0; k<n/3; k++) {
-		for (i=0;i<n/3; i++) {
-			printf("***");
-		}
-		printf("\n");
-		for (i=0;i<n/3; i++) {
-			printf("* *");
-		}
-		printf("\n");
-		for (i=0;i<n/3; i++) {
-			printf("***");
+	sss(n/3);
+	printf("\n");
+	l++;
+	sbs(n/3);
+	printf("\n");
+	l++;
+	sss(n/3);
+	printf("\n");
+	l++;
+	for (i=2; i<=n/3+1; i++) {
+		if (i%3==0) bbb(1);
+		else {
+			if (l%3==0)sss(1);
+			else sbs(1);
 		}
 	}
 

fc8d4b12345cbf5e8ff459f1934b12dc1e68ce32
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-05-20 14:46:06 +0900

    Update 2805.c

--- 2805.c
+++ 2805.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+<<<<<<< HEAD
 int main (int argc, char*argv[]){
 	int n, m, a[1000000], i, ii, sum, max=0;
 
@@ -22,5 +23,26 @@ int main (int argc, char*argv[]){
 	}
 	printf("%d", i-1);
 	
+=======
+int main() {
+	int h[1000], n, m, i, ii, max=0, min=100000000;
+	long long int sum;
+
+	scanf("%d%d", &n, &m);
+
+	for (i=0; i<n; i++) {
+		scanf("%d", &h[i]);
+		if (h[i] > max) max = h[i];
+		if (h[i] < min) min = h[i];
+	}
+
+	for (i=max-m/n; i>0; i--){
+		sum=0;
+		for (ii=0; ii<n; ii++) sum+=(h[ii] > i) ? h[ii]-i : 0;
+		if (sum >= m) break;
+	}
+	printf("%d", i);
+
+>>>>>>> 465b1ab242b072699e29971528245b07528a3aea
 	return 0;
 }

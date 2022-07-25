118bff77dcfff3a19456053f1cf4f74cef8ab4e8
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-07-25 18:39:02 +0900

    Update 2447.c

--- 2447.c
+++ 2447.c
@@ -1,46 +1,22 @@
 #include <stdio.h>
-#include <math.h>
 
-int row=1, column=1, r=9;
-void line() {
-	printf("%d \n", row);
-	row++;
+void star(int i, int j, int n) {
+    if (i%3==1 && j%3==1) {
+        printf(" ");
+        return;
+    }
+    else if (n==1) {
+        printf("*");
+        return;
+    }
+    else star (i/3, j/3, n/3);
 }
-
-void sprint(double n, int k) {
-	int i, l=1, j, kk;
-
-	kk=pow(3,k)/1;
-
-	for (i=0; i<pow(3,k)/3; i++) {
-	if ((row>=(r+1)/2-r/9 && row<=(r+1)/2+r/9) && (column+1)%3==0) {
-		for (j=0; j<r/9; j++) printf("   ");
-	}
-	else if ((row+1)%3==0) printf("* *");
-	else printf("***");
-		column++;
-	}
-	if (row==(r+1)/2+1) r*=3;
-	line();
-	column=1;
-}
-
 int main() {
-	int i, j, m, k, l=1;
-	double n, kt;
-
-	// n+1 해서 반 나누고 k 구하면 깔끔할듯
-	scanf("%lf", &n);
-	kt = log(n) / log(3.0);
-	k = kt / 1;
-	n = ++n/2.0;
-
-	for (j=0; -1<=j && j<=n; j+=l) {
-		sprint(n, k);
-		if (j==n) l=-2;
-	}
-	
-
-	
-	return 0;
+    int n, i, j;
+    scanf("%d", &n);
+    
+    for (i=0; i<n; i++) {
+        for (j=0; j<n; j++) star(i, j, n);
+        printf("\n");
+    }
 }

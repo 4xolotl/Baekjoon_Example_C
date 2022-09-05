169189b11e34e16f8432b6feb404fe82a3bdf868
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-09-05 23:54:09 +0900

    Update 10994.c

--- 10994.c
+++ 10994.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 
-void star(int x, int y, int n) {
-	if (x%2 == 0 && y%2 == 0) {
-		printf(" ");
-		return;
-	}
-	else if (n==1) {
-		printf("*");
-		return;
-	}
-	else star(x/3, y/3, n/3);
-}
-
 int main() {
 	int i, n, x, y, s;
 
@@ -19,8 +7,12 @@ int main() {
 
 	for (y=0; y<4*n-3; y++) {
 		for (x=0; x<4*n-3; x++) {
-			if ()
-			printf("*");
+			if (y%2==0) {
+				printf("*");
+			}
+			else {
+				printf(" ");
+			}
 		}
 		printf("\n");
 	}

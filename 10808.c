d3f8394537684973ea79379ac17cb6b23a82ffa5
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-10-12 18:21:57 +0900

    백준 10808번

--- 10808.c
+++ 10808.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
-main(){int a[26]={0},i=-26;char c=getchar();while(c!=EOF){a[c-97]++;c=getchar();}while(i++)printf("%d ",a[i+25]);}
+
+int main() {
+	int a[100]={0}, i=-26;
+	char c=getchar();
+	
+	while (c!=EOF) {
+		a[c-97]++;
+		c=getchar();
+	}
+	while (i++){
+		printf("%d ", a[i+25]);
+	}
+
+	return 0;
+}
